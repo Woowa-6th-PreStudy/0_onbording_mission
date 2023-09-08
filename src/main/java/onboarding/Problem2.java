d3150b109368ff8a31bcb97da3e2be2b2dc6eb87
hyuna <monicajo0@naver.com>
2023-09-08 13:07:08 +0900

    feat: Problem2 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,39 @@
 package onboarding;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return makeNewPassword(cryptogram);
+    }
+
+    public static String makeNewPassword(String cryptogram){
+        Deque newChars = removeDuplicatedChar(cryptogram);
+
+        String newPassword = "";
+        while(!newChars.isEmpty()){
+            newPassword += newChars.pollFirst();
+        }
+
+        return newPassword;
+    }
+
+    public static Deque<Character> removeDuplicatedChar(String cryptogram){
+        Deque<Character> deque = new ArrayDeque<>();
+
+        int cryptogramLength = cryptogram.length();
+        for(int i=0;i<cryptogramLength;i++){
+            char ch = cryptogram.charAt(i);
+
+            if(deque.isEmpty() || deque.peekLast()!=ch){
+                deque.add(ch);
+            }
+            else if(deque.peekLast() == ch){
+                deque.pollLast();
+            }
+        }
+
+        return deque;
     }
 }
