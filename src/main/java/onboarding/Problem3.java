36f7b23b1be36013e33467e4a722fbb1e895a3c9
hyuna <monicajo0@naver.com>
2023-09-08 13:07:12 +0900

    feat: Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=3;i<=number;i++){
+            answer += countSlap(i);
+        }
+
         return answer;
+    }
+
+    public static int countSlap(int number){
+        int cnt = 0;
+
+        while(number>0){
+            int digit = number%10;
+
+            if(digit==3 || digit==6 || digit==9){
+                cnt++;
+            }
+
+            number /= 10;
+        }
+
+        return cnt;
     }
 }
