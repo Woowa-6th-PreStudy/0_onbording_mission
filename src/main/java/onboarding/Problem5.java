288e8afe7e7bac67a079191969d3cc3e19b5f827
hyuna <monicajo0@naver.com>
2023-09-08 13:07:25 +0900

    feat: Problem5 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    public static int[] prices = {50000,10000,5000,1000,500,100,50,10,1};
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return exchangeMoney(money);
+    }
+
+    public static List<Integer> exchangeMoney(int money){
+        List<Integer> counts = new ArrayList<>();
+
+        for(int i=0;i<prices.length;i++){
+            int price = prices[i];
+            counts.add(money / price);
+
+            money %= price;
+        }
+
+        return counts;
     }
 }
