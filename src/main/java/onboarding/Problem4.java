f199abe82292ad83082d92517b70527aa86354b5
hyuna <monicajo0@naver.com>
2023-09-08 13:07:19 +0900

    feat: Problem4 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,25 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        int wordLength = word.length();
+        for(int i=0;i<wordLength;i++){
+            answer += Character.toString(changeChar(word.charAt(i)));
+        }
+
         return answer;
+    }
+
+    public static char changeChar(char originalChar){
+        char changedChar = originalChar;
+
+        if('a'<=originalChar && originalChar<='z'){
+            changedChar = (char) ('z'-originalChar+'a');
+        }
+        else if('A'<=originalChar && originalChar<='Z'){
+            changedChar = (char) ('Z'-originalChar+'A');
+        }
+
+        return changedChar;
     }
 }
